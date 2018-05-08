ac651f62e91cfd018dab1768e2c89580ee713b74
SatyamShivamShankar <39099208+SatyamShivamShankar@users.noreply.github.com>
2018-05-09 02:31:15 +0530

    Update Solution.cpp

--- C - 2D Arrays/Solution.cpp
+++ C - 2D Arrays/Solution.cpp
@@ -1,27 +1,32 @@
-#include <iostream>
-#include <vector>
 
+#include <iostream>
 using namespace std;
 
-int main() {
-    vector<vector<int>> arr(6, vector<int>(6));
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
-            cin >> arr[i][j];
+
+int main(){
+
+    int arr [6][6];
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            cin>>arr[i][j];
         }
     }
-
-    int max = -9 * 7;
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
-            if (j + 2 < 6 && i + 2 < 6) {
-                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
-                if (sum > max) max = sum;
-            }
-        }
+    int k=-63;
+   // int i=2 ,j=0;
+        for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            int sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+            if(sum>k)
+                k=sum;
+       }
+       
     }
-
-    cout << max;
-
+    
+    cout<<k;
     return 0;
 }
+
